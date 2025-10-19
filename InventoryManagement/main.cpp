cafd27f503f7edc8753d5bc89d497d03bd9e7583
Ethan Lockwood <lockwood.ethan@yahoo.com>
2025-10-18 20:29:40 -0400

    Switched dev environment to Linux

--- InventoryManagement/main.cpp
+++ InventoryManagement/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <stdio.h>
 #include <sqlite3.h>
 #include <string>
--- InventoryManagement/sqlite_database.cpp
+++ InventoryManagement/sqlite_database.cpp
@@ -1,4 +1,6 @@
 #include <sqlite3.h>
+#include <cstring>
+#include <cmath>
 #include <iostream>
 #include <stdio.h>
 #include <string>
